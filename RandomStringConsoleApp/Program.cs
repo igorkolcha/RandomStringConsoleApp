1d982f0a7e2820ef68857cca04134884118b087c
igor.kolcha <igor.kolcha@list.ru>
2023-07-05 11:28:03 +0300

    Created a text file

--- RandomStringConsoleApp/Program.cs
+++ RandomStringConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace RandomStringConsoleApp
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            List<string> myWords = new List<string>();
+            myWords.AddRange(File.ReadAllLines("text.txt"));//считываем из файла слова
+            Console.WriteLine("String list:");
+            foreach (string mW in myWords)
+                Console.WriteLine("{0}", mW);
+            Console.WriteLine("---------------------------");
+            int randomString = random.Next(myWords.Count);
+            Console.WriteLine("Random string: {0}", myWords[randomString]);
+            Console.Read();
         }
     }
 }
