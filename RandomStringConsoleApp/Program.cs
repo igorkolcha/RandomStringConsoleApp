23a41d4fdd377418bf8ae12a69930a566e04278c
igor.kolcha <igor.kolcha@list.ru>
2024-05-17 18:10:07 +0300

    random output of five words

--- RandomStringConsoleApp/Program.cs
+++ RandomStringConsoleApp/Program.cs
@@ -12,25 +12,50 @@ namespace RandomStringConsoleApp
         static void Main(string[] args)
         {
             Random random = new Random();
-
+            int y = 1;
+            StreamWriter streamWriter = new StreamWriter("text2.txt");
             List<string> myWords = new List<string>();
-            myWords.AddRange(File.ReadAllLines("text.txt"));//считываем из файла слова
+            myWords.AddRange(File.ReadAllLines("english.txt"));//считываем из текстового файла слова
             Console.WriteLine("String list:");
             foreach (string mW in myWords)
-                Console.WriteLine("{0}", mW);
+                Console.WriteLine("{0}   {1}", y++, mW);
 
-            Console.WriteLine("--------------string random-------------------");
+            Console.WriteLine("--------------String random-------------------");
             int randomString = random.Next(myWords.Count);
             Console.WriteLine("Random string: {0}", myWords[randomString]);
 
-            Console.WriteLine("--------------Five string random-------------");
-            Random rnd = new Random();
-            for(int i = 0; i < 5; i++)
+            Console.WriteLine("--------------120 string random-------------");
+
+            //Random rnd = new Random();
+
+            for(int i = 0; i < 120; i++)
             {
-                int indexRnd = rnd.Next(myWords.Count);
-                Console.WriteLine("{0}", myWords[indexRnd]);
+                //int indexRnd = rnd.Next(myWords.Count);
+                int indexRnd = random.Next(myWords.Count);//mew
+                    Console.WriteLine("{0}", myWords[indexRnd]);
+                    //записываем в текстовый файл text2 рандомные слова
+                    streamWriter.WriteLine(myWords[indexRnd]);
             }
-            Console.Read();
+            streamWriter.Close();
+
+            void Duplicates()
+            {
+                //Проверка повторяющихся слов в текстовом файле text2
+                Console.WriteLine("-------------------Duplicates---------------");
+
+                List<string> newWords = new List<string>();
+                newWords.AddRange(File.ReadAllLines("text2.txt"));
+
+                var duplicates = newWords.GroupBy(x => x)
+                    .Where(g => g.Count() > 1)
+                    .Select(z => new { Item = z.Key, Count = z.Count() })
+                    .ToList();
+                Console.WriteLine(String.Join("\n", duplicates));               
+            }
+
+            Duplicates();
+
+            Console.Read();           
         }
     }
 }
